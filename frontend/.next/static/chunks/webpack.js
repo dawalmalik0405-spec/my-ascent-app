b3b05d8f8bc65360be69104cc35a3310985ac42a
dawalmalik0405-spec <dawalmalik0405@gmail.com>
2026-05-16 09:29:00 +0530

    Update app routing and manifests to include new incidents and research pages. Remove obsolete _not-found page references and modify encryption key for enhanced security. Update various binary cache files for improved performance.

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "69fb12be8da138a0"; }
+/******/ 		__webpack_require__.h = function() { return "fe2723db876e1f54"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
